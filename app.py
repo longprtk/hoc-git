626d82ebe37974117c294048262ec110c27a4e6d
longprtk <longyeuai22@gmail.com>
2026-05-15 10:50:31 +0700

    them bai test và git

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
-print("Xin chao git")
-print("Toi dang học dev")
+def tinh_tong(a, b):
+    return a + b
+
+def tinh_hieu(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("ket qua tong: ",tinh_tong(5, 3))
+    print("ket qua hieu: ",tinh_hieu(5, 3))
---
+++ test_app.py
@@ -0,0 +1,5 @@
+from app import *
+def test_tinh_tong():
+    assert tinh_tong(5, 3) == 8
+def test_tinh_hieu():
+    assert tinh_hieu(5, 3) == 2
